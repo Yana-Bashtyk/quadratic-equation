7fed228b4e47bbe21a0f186376eb67fa156c8336
Yana Bashtyk <Yana_bashtyk@epam.com>
2018-09-29 19:30:06 +0300

    second run of refactoring to solution

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 module.exports = function solveEquation(equation) {
-  let a, b, c, D, x1, x2;
+  let a, b, c, d, x1, x2;
   const arrSplitted = equation.split(' ');
   a = Number(arrSplitted[0]);
   b = Number(arrSplitted[3]+arrSplitted[4]);
   c = Number(arrSplitted[7]+arrSplitted[8]);
   const arrResult = [];
   if (a!=0){
-    D = Math.pow(b,2) - (4*a*c);
-    x1 = Math.round((-b + Math.sqrt(D))/(2*a));
-    x2 = Math.round((-b - Math.sqrt(D))/(2*a));
+    d = Math.pow(b,2) - (4*a*c);
+    x1 = Math.round((-b + Math.sqrt(d))/(2*a));
+    x2 = Math.round((-b - Math.sqrt(d))/(2*a));
     arrResult.push(x1, x2);
 
     arrResult.sort((x, y) =>  x - y);
